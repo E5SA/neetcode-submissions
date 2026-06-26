19f1d7e6a033ce694a08df26a6b38c2b808c5480
E5SA <77307810+E5SA@users.noreply.github.com>
2026-06-26 10:47:57 +1000

    Add: products-of-array-discluding-self - submission-0

---
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.cpp
@@ -0,0 +1,28 @@
+class Solution {
+   public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        int total_product = reduce(nums.begin(), nums.end(), 1, multiplies<>());
+        vector<int> products;
+        for (size_t i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] == 0)
+            {
+                int zero_product = 1;
+                for (size_t j = 0; j < nums.size(); j++)
+                {
+                    if (j == i)
+                    {
+                        continue;
+                    }
+                    zero_product *= nums[j];
+                }
+                products.push_back(zero_product);
+            }
+            else
+            {
+                products.push_back(total_product / nums[i]);
+            }
+        }
+        return products;
+    }
+};
