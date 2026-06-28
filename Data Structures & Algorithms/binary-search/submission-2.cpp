af59e5df348f058270fa050b22add8a80c28af31
E5SA <77307810+E5SA@users.noreply.github.com>
2026-06-29 00:11:43 +1000

    Add: binary-search - submission-2

---
+++ Data Structures & Algorithms/binary-search/submission-2.cpp
@@ -0,0 +1,17 @@
+class Solution {
+   public:
+    int search(vector<int>& nums, int target) {
+        if (!ranges::contains(nums, target)) return -1;
+
+        size_t left = 0, right = nums.size() - 1, middle;
+        while (right - left > 1) {
+            middle = (left + right) / 2;
+            if (nums[middle] == target) return middle;
+            if (nums[middle] > target)
+                right = middle;
+            else
+                left = middle;
+        }
+        return (nums[left] == target) ? left : right;
+    }
+};
