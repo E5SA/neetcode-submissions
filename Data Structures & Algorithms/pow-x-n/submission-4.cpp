8bb8a35389098144e7dd4aa3408ca9153534acce
E5SA <77307810+E5SA@users.noreply.github.com>
2026-06-29 23:00:44 +1000

    Add: pow-x-n - submission-4

---
+++ Data Structures & Algorithms/pow-x-n/submission-4.cpp
@@ -0,0 +1,8 @@
+class Solution {
+   public:
+    double myPow(double x, int n) {
+        double base = x, result = 1;
+        for (size_t i = 0; i < abs(n); ++i) result *= (n >= 0 || x == 0) ? base : 1 / base;
+        return result;
+    }
+};
