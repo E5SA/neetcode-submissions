6d15cf1db2058d5c81c436065f5d3d378989ac94
E5SA <77307810+E5SA@users.noreply.github.com>
2026-07-01 16:13:55 +1000

    Add: anagram-groups - submission-0

---
+++ Data Structures & Algorithms/anagram-groups/submission-0.cpp
@@ -0,0 +1,18 @@
+class Solution {
+   public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        array<int, 26> letter_prime = {5,  71, 37, 29, 2,  53, 59, 19, 11, 83, 79, 31, 43,
+                                       13, 7,  67, 97, 23, 17, 3,  41, 73, 47, 89, 61, 101};
+        unordered_map<int, vector<string>> anagrams;
+        for (string str : strs) {
+            int product = 1;
+            for (char ch : str) {
+                product *= letter_prime[ch - 'a'];
+            }
+            anagrams[product].push_back(str);
+        }
+        vector<vector<string>> result;
+        for (const auto& pair : anagrams) result.push_back(pair.second);
+        return result;
+    }
+};
