12aca9b3419ef510d957dd559eb50c35d636823d
E5SA <77307810+E5SA@users.noreply.github.com>
2026-07-02 00:10:24 +1000

    Add: combinations-of-a-phone-number - submission-0

---
+++ Data Structures & Algorithms/combinations-of-a-phone-number/submission-0.cpp
@@ -0,0 +1,22 @@
+class Solution {
+   public:
+    vector<string> letterCombinations(string digits) {
+        if (digits.empty()) return {};
+
+        const array<string, 8> charset = {"abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
+
+        vector<string> combinations{""};
+
+        for (char digit : digits) {
+            vector<string> next;
+            next.reserve(combinations.size() * charset[digit - '2'].size());
+
+            for (const auto& prefix : combinations)
+                for (char c : charset[digit - '2']) next.push_back(prefix + c);
+
+            combinations = std::move(next);
+        }
+
+        return combinations;
+    }
+};
