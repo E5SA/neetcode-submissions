aa689a5adf41c3382362b1e288737d824979f2bd
E5SA <77307810+E5SA@users.noreply.github.com>
2026-07-02 18:12:08 +1000

    Add: merge-strings-alternately - submission-4

---
+++ Data Structures & Algorithms/merge-strings-alternately/submission-4.cpp
@@ -0,0 +1,14 @@
+class Solution {
+   public:
+    string mergeAlternately(string word1, string word2) {
+        string merged_string;
+        int p1 = 0, p2 = 0;
+        while (p1 < word1.length() && p2 < word2.length()) {
+            merged_string.push_back(word1[p1++]);
+            merged_string.push_back(word2[p2++]);
+        }
+        merged_string.append(word2, p2);
+        merged_string.append(word1, p1);
+        return merged_string;
+    }
+};
