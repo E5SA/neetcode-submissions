b3983d8e8456f599b25745a58f40f3e77584be94
E5SA <77307810+E5SA@users.noreply.github.com>
2026-07-04 23:18:51 +1000

    Add: search-insert-position - submission-5

---
+++ Data Structures & Algorithms/search-insert-position/submission-5.cpp
@@ -0,0 +1,16 @@
+class Solution {
+   public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1;
+        while (left <= right) {
+            int middle = left + (right - left) / 2;
+            if (target == nums[middle])
+                return middle;
+            else if (target < nums[middle])
+                right = middle - 1;
+            else if (target > nums[middle])
+                left = middle + 1;
+        }
+        return left;
+    }
+};
