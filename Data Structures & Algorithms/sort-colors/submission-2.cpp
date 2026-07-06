ff7913c543d12cee95fd44f4ec8e29b0d7afaa7a
E5SA <77307810+E5SA@users.noreply.github.com>
2026-07-06 16:12:37 +1000

    Add: sort-colors - submission-2

---
+++ Data Structures & Algorithms/sort-colors/submission-2.cpp
@@ -0,0 +1,19 @@
+class Solution {
+   public:
+    void sortColors(vector<int>& nums) {
+        int left = 0, current = 0, right = nums.size() - 1;
+
+        while (current <= right) {
+            if (nums[current] == 0) {
+                swap(nums[left], nums[current]);
+                left++;
+                current++;
+            } else if (nums[current] == 1)
+                current++;
+            else {
+                swap(nums[current], nums[right]);
+                right--;
+            }
+        }
+    }
+};
